c8a14c3090a301d3c624a4417faf15a7ff370dca
NKKNix <supawit.thongrueng@gmail.com>
2025-03-17 00:02:02 +0700

    Add start script to package.json and fix port variable in server.js logging

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ sequelize
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${port}`);
   }
 )
   
